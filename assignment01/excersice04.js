19a282ed76b9861abdc88a6d58e3aecd774ed3cb
sahilkumar <sahilkv14@gmail.com>
2026-02-09 00:27:51 +0530

    [Sahil].nodejs-programming Added simple todo api using http module

--- assignment01/excersice04.js
+++ assignment01/excersice04.js
@@ -2,76 +2,73 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(req,res){
-
-    //reading todos
+    res.setHeader("Content-Type", "application/json");
+    // READ todos
     if(req.method === "GET" && req.url === "/todos"){
         let data = fs.readFileSync("todo.json","utf-8");
         res.end(data);
     }
 
-    //create todo
+    // CREATE todo
     else if(req.method === "POST" && req.url === "/create"){
         let body = "";
-        req.on("data",function(chunk){
+
+        req.on("data", function(chunk){
             body += chunk;
         });
 
-        req.on("end",function(){
-            let data = fs.readFileSync("todo.json","utf-8",function(err,data){
-            let todos = JSON.parse(data);
+        req.on("end", function(){
+            let todos = JSON.parse(fs.readFileSync("todo.json","utf-8"));
 
             let newTodo = {
-                id :Date.now(),
-                title:JSON.parse(body).title,
-                status:"pending"
+                id: Date.now(),
+                title: JSON.parse(body).title,
+                status: "pending"
             };
             todos.push(newTodo);
-
-            fs.writeFileSync("todo.json",JSON.stringify(todos,null,2), function(){
-                res.end(JSON.stringify(newTodo));
-            });
-            });
-
+            fs.writeFileSync("todo.json", JSON.stringify(todos, null, 2));
+            res.end(JSON.stringify(newTodo));
         });
     }
 
-    //updating todo
+    // UPDATE todo
     else if(req.method === "PUT" && req.url.startsWith("/todos/")){
-        let id  = Number(req.url.split("/")[2]);
+        let id = Number(req.url.split("/")[2]);
         let body = "";
 
-        req.on("data",function(chunk){
+        req.on("data", function(chunk){
             body += chunk;
-        })
+        });
 
-        req.on("data",function(){
+        req.on("end", function(){
             let todos = JSON.parse(fs.readFileSync("todo.json","utf-8"));
             let update = JSON.parse(body);
 
-            for(let i = 0;i<todos.length;i++){
-                if(todos[i].id===id){
+            for(let i = 0; i < todos.length; i++){
+                if(todos[i].id === id){
                     if(update.title !== undefined) todos[i].title = update.title;
                     if(update.status !== undefined) todos[i].status = update.status;
                 }
             }
-            fs.writeFileSync("todo.json",JSON.stringify(todos));
-            res.end(JSON.stringify({message:"updated"}));
+
+            fs.writeFileSync("todo.json", JSON.stringify(todos, null, 2));
+            res.end(JSON.stringify({ message: "updated" }));
         });
     }
 
-    //delete todo
-    else if(req.method === "DELETE"&&req.url.startsWith("/todos")){
+    // DELETE todo
+    else if(req.method === "DELETE" && req.url.startsWith("/todos/")){
         let id = Number(req.url.split("/")[2]);
 
         let todos = JSON.parse(fs.readFileSync("todo.json","utf-8"));
-        todos = todos.filter((value) => value.id != id);
+        todos = todos.filter(t => t.id !== id);
 
-        fs.writeFileSync("todo.json",JSON.stringify(todos));
-        res.end(JSON.stringify({message:"Deleted"}));
+        fs.writeFileSync("todo.json", JSON.stringify(todos, null, 2));
+        res.end(JSON.stringify({ message: "Deleted" }));
     }
     else{
-        res.statusCode = 500;
-        res.end(JSON.stringify({message:"internal server error"}));
+        res.statusCode = 404;
+        res.end(JSON.stringify({ message: "Not found" }));
     }
 }).listen(8080);
 
