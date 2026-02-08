dc690f469c989b02f9219c2d3b5330f812122440
sahilkumar <sahilkv14@gmail.com>
2026-02-09 00:28:55 +0530

    [Sahil].nodejs-programming Added index.js file which is enter point of assignment01

--- assignment01/index.js
+++ assignment01/index.js
@@ -1,13 +1,14 @@
 let {ReadLog} = require('./Function01');
 let {CapitaliseString, ReverseString, CountoFVowels} = require('./stringutils');
 let {UseSetTimeout, UseSetInterval, useSetImmediate} = require('./Function02');
+let {SystemInfo} = require('./excersice03');
 
 ReadLog();
 // console.log(CapitaliseString("hello world"));
 // console.log(ReverseString("hello world"));
 // console.log((CountoFVowels("hello world")));
 
-
+SystemInfo();
 // useSetImmediate();
 // UseSetTimeout();
 // UseSetInterval();
