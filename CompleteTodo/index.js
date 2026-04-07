81fc569f50c040f692dfd8b2d5f266f62b1ffdeb
sahilkumar <sahilkv14@gmail.com>
2026-04-07 14:04:49 +0530

    [Sahil].todo updated index.js

--- CompleteTodo/index.js
+++ CompleteTodo/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 import userSchema from "./schema/user.schema.js"
 import TodoSchema from "./schema/todo.schema.js"
+import usersignup from "./service/user.signupservice.js"
+import userSignupMidware from "./middleware/user.signupmidware.js"
 dotenv.config()
 
 const App = express()
@@ -11,8 +13,12 @@ mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log("successfully connected to mongoDB"))
 .catch(()=>console.log("some error occured in connecting to mongoDB"))
 
-userSchema()
-TodoSchema()
+
+App.post('/signup',userSignupMidware,usersignup)
+
+
+// userSchema()
+// TodoSchema()
 App.listen(process.env.PORT || 8000,()=>{
     console.log("server connected at localhost:8080")
 })
