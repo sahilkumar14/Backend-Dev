661e13cf6e590f0c65238b28a5b6db6759c315d5
sahilkumar <sahilkv14@gmail.com>
2026-04-09 14:26:09 +0530

    [Sahil].todo updated schema for todo

--- CompleteTodo/schema/todo.schema.js
+++ CompleteTodo/schema/todo.schema.js
@@ -1,30 +1,26 @@
 import mongoose from "mongoose";
 
-export default function TodoSchema(){
-    try {
-        let TodoSchema = new mongoose.Schema({
-            title:{
-                type:String,
-                trim:true,
-                required:true
-            },
-            description:{
-                type:String,
-                trim:true,
-                required:true
-            },
-            status:{
-                type:String,
-                trim:true,
-            },
-            user_id:{
-                type:mongoose.Schema.ObjectId,
-                ref:"user"
-            }
-        })
-        let todo = mongoose.model('todo',TodoSchema)
 
-    } catch (error) {
-        console.log(error)
+let TodoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    status: {
+        type: String,
+        trim: true,
+        enum:["pending","ongoing","completed"]
+    },
+    user_id: {
+        type: mongoose.Schema.ObjectId,
+        ref: "user"
     }
-}
+},{timestamps:true})
+
+export const Todo = mongoose.model("todo",TodoSchema)
