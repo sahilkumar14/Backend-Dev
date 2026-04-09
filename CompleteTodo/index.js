2f62ee7d2bd89d60a2288512da3fbbac266ab0ed
sahilkumar <sahilkv14@gmail.com>
2026-04-09 14:27:28 +0530

    [Sahil].todo updated index.js

--- CompleteTodo/index.js
+++ CompleteTodo/index.js
@@ -1,22 +1,32 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-import usersignup from "./service/user.signupservice.js"
-import userSignupMidware from "./middleware/user.signupmidware.js"
+import cookieParser from "cookie-parser"
+import {rateLimit} from 'express-rate-limit'
+import router from "./routes/user.route.js"
+
 dotenv.config()
 
 const App = express()
 App.use(express.json())
+App.use(cookieParser)
+
 mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log("successfully connected to mongoDB"))
 .catch(()=>console.log("some error occured in connecting to mongoDB"))
 
 
-App.post('/signup',userSignupMidware,usersignup)
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit:10,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+    ipv6Subnet: 56
+})
+App.use(limiter)
 
+App.use('/',router)
 
-// userSchema()
-// TodoSchema()
 App.listen(process.env.PORT || 8000,()=>{
     console.log("server connected at localhost:8080")
 })
