439214160d0e64bb1387ebc1e65ccb9adace9966
sahilkumar <sahilkv14@gmail.com>
2026-04-10 21:27:52 +0530

    [sahil].session-cookies added multi-step registration form that store user input in session

--- exercise02/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import validateYearQuery from "./middleware/validateYearQuery.js";
-
-const app = express();
-
-app.get("/year", validateYearQuery, (req, res) => {
-  res.json({ message: "Valid request" });
-});
