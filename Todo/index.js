638e59a74c104dbeeee97d7177a8e37788eb7cb1
sahilkumar <sahilkv14@gmail.com>
2026-04-23 00:01:19 +0530

    [Sahil].rest-security added rest-security questions

--- Todo/index.js
+++
@@ -1,9 +0,0 @@
-import registerUser from "./srcCode/registerUser.js";
-import loginUSer from "./srcCode/loginUser.js";
-import createTodo from "./srcCode/createTodo.js";
-import deleteTodo from "./srcCode/deleteTodo.js";
-// console.log(registerUser("sahil","sahil@gmail.com","1234"))
-// console.log(loginUSer("sahil@gmail.com","1234"))
-// console.log(createTodo("sahil","this is first todo created for testing create todo function"))
-console.log(createTodo("sahil","delete function implemented"))
-// console.log(deleteTodo("sahil"))
